c9f2bfb71fca33292dd575d3f9e1acb919f49734
chuyao <1209394810@qq.com>
2020-10-10 10:07:36 +0800

    2020-10-10

--- Leetcode/editor/cn/[142]环形链表 II.cpp
+++ Leetcode/editor/cn/[142]环形链表 II.cpp
@@ -50,6 +50,22 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+
+//相遇时：
+//slow走过的路程：x + y + n(y+z), n代表slow绕了n圈
+//fast走过的路程：x + y + m(y+z)，m代表fast饶了m圈
+//m > n
+//因为fast速度是slow两倍：
+//2(x + y + n(y + z)) = x + y + m(y + z)
+//x + y = (m - 2n)(y + z)
+//x = (m - 2n)(y + z) - y
+//y + z就是1圈，假设 o = m-2n，o是一个正整数，那么 x = o(y + z) -y
+//如果o = 1，那么 x = z，
+//如果o > 1，那么 x = (o-1)(y+z)- y + (y + z) , x = (o-1)(y+z) + z，即x的长度为o-1圈加上z
+//所以，从第一阶段获得的相遇点，走过x距离机会到达环的起点。
+
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
