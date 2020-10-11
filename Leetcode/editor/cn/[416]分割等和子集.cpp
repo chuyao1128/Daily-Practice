d575cb28f90608494cd30e8c244ae626f4ec32c8
chuyao1128 <1209394810@qq.com>
2020-10-11 15:38:50 +0800

    2020-10-11

---
+++ Leetcode/editor/cn/[416]分割等和子集.cpp
@@ -0,0 +1,69 @@
+//给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+//
+// 注意:
+//
+//
+// 每个数组中的元素不会超过 100
+// 数组的大小不会超过 200
+//
+//
+// 示例 1:
+//
+// 输入: [1, 5, 11, 5]
+//
+//输出: true
+//
+//解释: 数组可以分割成 [1, 5, 5] 和 [11].
+//
+//
+//
+//
+// 示例 2:
+//
+// 输入: [1, 2, 3, 5]
+//
+//输出: false
+//
+//解释: 数组不能分割成两个元素和相等的子集.
+//
+//
+//
+// Related Topics 动态规划
+// 👍 498 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    bool canPartition(vector<int> &nums) {
+        int n = nums.size();
+        if (n < 2) {
+            return false;
+        }
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (auto num : nums) {
+            sum += num;
+        }
+        int target = sum / 2;
+        if ((sum % 2) || (nums.back() > target)) {
+            return false;
+        }
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < target + 1; j++) {
+                if (j >= nums[i]) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n - 1][target];
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
